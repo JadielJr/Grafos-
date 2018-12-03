36a6120667fae0e81e6fd5c452e88af2f4c84f5b
JadielJr <39445244+JadielJr@users.noreply.github.com>
2018-12-03 10:35:23 -0300

    Update grafo.cpp

--- grafo.cpp
+++ grafo.cpp
@@ -1,16 +1,16 @@
-#include <cstdlib>
-#include <cstdio>
-#include "grafo.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include "grafo.h"
 
 /*
-/////////////////////////////////////////////////////////////////////
-|  Função que cria uma adjacência  para ser inserida em uma         |
-|	lista de adjacências;                     	            |
-|					                            |
-|	Parâmetros da função:				            |
-|	-Vertice onde chega a aresta;                               |
-|	-peso da aresta/ou zero, caso a aresta não seja ponderada;  |
-/////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+|  Função que cria uma adjacência  para ser inserida em uma    |
+|	lista de adjacências;                     				   |
+|															   |
+|	Parâmetros da função:									   |
+|	-Vertice onde chega a aresta;                              | 
+|	-peso da aresta/ou zero, caso a aresta não seja ponderada; |
+////////////////////////////////////////////////////////////////
 */
 
 Adj* criaAdj(int v, int peso)
@@ -32,13 +32,13 @@ Adj* criaAdj(int v, int peso)
 }
 
 /*
-///////////////////////////////////////////////////////////////////////////////////////////////////
-|  Função que cria uma aresta, ou seja ela insere na lista de adjacências uma Adj retornada pela  |
-|  função criaAjd;                                                                                |
-|                                                                                                 |
-|  OBS:para criar um grafo não direcional basta chamar a função duas vezes:a primeria             | 
-|  vez com VI indo pra VF e a segunda com VF indo pra VI;                                         |
-///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+|  Função que cria uma aresta, ou seja ela insere na lista de adjacências uma Adj retornada pela |
+|  função criaAjd;                                                                               |
+|                                                                                                |
+|  OBS:para criar um grafo não direcional basta chamar a função duas vezes:a primeria            |
+|  vez com VI indo pra VF e a segunda com VF indo pra VI;                                        |
+//////////////////////////////////////////////////////////////////////////////////////////////////
 */
 
 int criaAresta(Grafo* gr,int vi, int vf, int peso)
@@ -82,7 +82,7 @@ Grafo* grafoCreate(int v)
 
         for(int i=0; i<v; i++)
         {
-            g->adj[i].cab = NULL;     /*Preenchendo o vetor todo com NUlL;(Significa que não há arestas ou vértices adjacentes);*/
+            g->adj[i].cab = NULL;     /*Preenchendo o vetor todo com NULL;(Significa que não há arestas ou vértices adjacentes);*/
             g->adj[i].dado = i;
         }
         return g;
@@ -124,7 +124,7 @@ void printGrafo(Grafo* gr)
 //////////////////////////////////////
 |  Função de visita em profundidade; |
 |                                    |
-|  Parâmetros da função:             |
+|  Parâmetros da função:             | 
 |  -grafo;                           |
 |  -índice do arranjo de vértices;   |
 |  -arranjo de cores dos vértices;   |
@@ -133,43 +133,43 @@ void printGrafo(Grafo* gr)
 */
 
 int visita(Grafo* gr,int i,int* cor,int key)
-{
+{	
     cor[i]= CINZA;
-    int flag;
+	int flag;
     Adj* v = gr->adj[i].cab;	/*variável setada no inicio da lista de adjacências para que ela possa interar essa lista;*/
-    if(gr->adj[i].dado==key || v->vertice == key)	/*verificando o vértice para saber se a busca foi bem sucedida;*/
-    {
-       return TRUE;
-    }
+	if(gr->adj[i].dado==key || v->vertice == key)	/*verificando o vértice para saber se a busca foi bem sucedida;*/
+	{
+		return TRUE;
+	}
     while(v)
     {
-       if(cor[v->vertice]==BRANCO)
-       {
-           flag = visita(gr,v->vertice,cor,key);
-           if(flag)
-           {
-               return TRUE;
-           }
-       }
-       v = v->prox;
+        if(cor[v->vertice]==BRANCO)
+        {
+            flag = visita(gr,v->vertice,cor,key);
+			if(flag)
+			{
+				return TRUE;
+			}
+		}
+        v = v->prox;
     }
     cor[i] = PRETO;
-    return FALSE;
+	return FALSE;
 }
 
 /*
-///////////////////////////////////////////
-|  Função de busca em profundidade(DFS);  |
-|                                         |
-|  Parâmetros da função:                  |
-|  -ponteiro para o grafo;                |
-|  -chave da busca;                       |
-///////////////////////////////////////////
+////////////////////////////////////////////
+|	Função de busca em profundidade(DFS);  |
+|                                          |
+|   Parâmetros da função:                  |
+|	-ponteiro para o grafo;				   |
+|	-chave da busca;					   |
+////////////////////////////////////////////
 */
 
 int profundidade(Grafo* gr, int key)
 {
-    int flag;
+	int flag;
     int cor[gr->numVert];
     for(int i=0; i<gr->numVert; i++)
     {
@@ -180,11 +180,100 @@ int profundidade(Grafo* gr, int key)
         if(cor[i]== BRANCO)
         {
             flag = visita(gr,i,cor,key);
-	    if(flag)
-	    {
-                return TRUE;
-            }
+			if(flag)
+			{
+				return TRUE;
+			}
         }
+    }
+	return FALSE;
+}
+
+
+/*
+///////////////////////////////////////////////////
+|	Função que cria uma fila, inicialmente vazia. |
+|                                                 |
+///////////////////////////////////////////////////
+*/
+
+SlList *sllCreate()
+{
+    SlList* l = (SlList*)malloc(sizeof(SlList));        //L é a variável que irá apontar para o primeiro elemento da lista.
+    if(l!=NULL){                                        //Testa se a alocação ocorreu.
+        l->first = NULL;                                //Inicializa a fila varia.
+        return l;
+    }
+    return NULL;
+}
+
+/*
+/////////////////////////////////////////////////////////
+|	Função Destroy, só destroi se a fila estiver vazia. |
+|	                                                    |
+/////////////////////////////////////////////////////////
+*/
+
+int sllDestroy(SlList* l)
+{
+    if(l->first == NULL){       //testa se a fila esta devidamente vazia.
+        free(l);                //Desaloca a lista.
+        return TRUE;
     }
     return FALSE;
 }
+
+/*
+/////////////////////////////////////////////////////////////////////////
+|	Função que insere um elemento dado pelo usuário em ultimo na lista. |
+|																	    |
+/////////////////////////////////////////////////////////////////////////
+*/
+
+int sllInsertLast(SlList* l,void* data)
+{
+    if(l!=NULL){
+        SlNode* aux;                                            //Variável que percorre a lista até o final.
+        SlNode* newnode = (SlNode*)malloc(sizeof(SlNode));      //Novo nó referente ao elemento que irá ser inserido.
+        if(newnode!=NULL){                                      //Teste da alocação.
+            newnode->data = data;                               //Novo nó aponta para o dado inserido.
+            newnode->next = NULL;                               //Novo nó aponta para NULL, indicando o fim da lista.
+            if(l->first!=NULL){                                 //se houver elementos na lista.
+                aux=l->first;
+                while(aux->next!=NULL){                         //Interação da lista vai até o final.
+                    aux = aux->next;
+                }                                               //neste ponto é garantido que aux está no último elemento da lista.
+                aux->next = newnode;                            //O último elemento aponta para o novo nó que passa a ser o último da lista.
+            }
+            else{                                               //Se não houver elementos na lista.
+                l->first = newnode;                             //O novo nó irá encabeçar a lista.
+            }
+            return TRUE;
+        }
+
+    }
+    return FALSE;
+}
+/*
+///////////////////////////////////////////////////
+|	Função que remove o último elemento da lista; |
+|	                                              |
+///////////////////////////////////////////////////
+*/
+
+void* sllRemoveLast(SlList* l)
+{
+	if(l!=NULL && l->first!=NULL)
+	{
+		SlNode* cur = l->first;
+		while(cur->next!=NULL)
+		{
+			cur = cur->next;
+		}
+		void* lixo = cur->data;
+		free(cur);
+		return lixo;
+	}
+	return NULL;
+}
+
