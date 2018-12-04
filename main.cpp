43f3c2cc6eff7339a3b1036cabf9067f4a51f8a0
JadielJr <39445244+JadielJr@users.noreply.github.com>
2018-12-04 07:59:11 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include <cstdio>
-#include <cstdlib>
-#include "grafo.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include "grafo.h"
 
-int main(void)
+int cmp(void* a, void* b);
+
+int main(void) 
 {
 	Grafo* gr = grafoCreate(5);
 	criaAresta(gr,0,1,0);
@@ -12,18 +14,35 @@ int main(void)
 	criaAresta(gr, 3, 1, 0);
 	criaAresta(gr, 4, 3, 0);
 	printGrafo(gr);
-
+	
 	for(int i=0; i<12;i++)
-	{
-            printf("Procurando o vertice v%d:\n",i);
-	    int aux = profundidade(gr, i);
-	    if(aux == TRUE)
-            {	
-                printf("Vertice encontrado.\n\n");
-            }
-	    else
-	    {
-                printf("Vertice nao encontrado.\n\n");
-            }
+	{	
+		printf("Procurando o vertice v%d:\n",i);
+		int aux = profundidade(gr, i);
+		if(aux == TRUE)
+		{
+			printf("Vertice encontrado.\n\n");
+		}
+		else
+		{
+			printf("Vertice nao encontrado.\n\n");
+		}
 	}
+}
+
+
+//Função que faz comparação entre dois elementos.
+int cmp(void* a, void* b)
+{
+    int *pa;            //Variável que irá converte o tipo do dado para int*.
+    int *pb;            //Variável que irá converte o tipo do dado para int*.
+                        //Realizando a conversão.
+    pa = (int*) a;
+    pb = (int*) b;
+    if(*pb == *pa){     //comparando os dados.
+        return TRUE;    //retorna true se for igual.
+    }
+    else{
+        return FALSE;   //retorna false se for diferente.
+    }
 }
